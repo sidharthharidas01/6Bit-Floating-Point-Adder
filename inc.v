9c22ef1cd241905d327918baf66d234ee2da6968
Sidharth Haridas <61899497+sidharthharidas01@users.noreply.github.com>
2022-05-10 12:15:12 +0530

    Update inc.v

--- inc.v
+++ inc.v
@@ -1,18 +1,10 @@
 `timescale 1ns / 1ps
-
-
-module inc(e,over,out); // in in in out
-    input  [1:0] e ;   // select line
-    input  over;  // 
-    output reg [1:0] out;  //output
-
-
-
-always @( e or over) begin
-    case (over)
-        0: out <= e;
-        1: out <= e+1;
-    endcase
-end
-
-endmodule  
+//Working
+module inc(
+    input wire [1:0] e,
+    input wire over,
+    output wire [1:0] out
+    );
+    
+    assign out = e+over;
+endmodule
